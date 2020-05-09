480ecc7637a9c1cdc3e9f64aa9aad11cf5ff4606
abhay <abhay.katheria1998@gmail.com>
2020-05-09 22:53:01 +0530

    Updates

---
+++ precache-manifest.978acf45d099d4e6e911641c0030d46a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b9d76581466a22a693f4de156450cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e388df30f4b4e01dac2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e875c8953fa281d9a18",
+    "url": "/static/js/2.09a11e94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09a11e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e388df30f4b4e01dac2",
+    "url": "/static/js/main.36f06a67.chunk.js"
+  },
+  {
+    "revision": "7eec3850126fe1d2ce11",
+    "url": "/static/js/runtime-main.321b3690.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
